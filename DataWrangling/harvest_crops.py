e35a5aa2c97e330d35028d42e79994e5d8df2c64
Shishir Jakati <shishir.jakati98@gmail.com>
2019-02-05 12:58:43 -0500

    Add nested folder support

--- DataWrangling/harvest_crops.py
+++ DataWrangling/harvest_crops.py
@@ -15,7 +15,7 @@ def harvest(images_parent_directory, annotations_parent_directory, crop_director
     ## Find the filenames of TIFF images, and then finds the corresponding annotation file
     ## Arguments
         ## images_parent_directory: Parent directory where the images live
-        ## annotations_parent_directory: Parent directory where the corresponding annotations live
+        ## annotations_parent_directory: Directory where the character annotations home folder is (contains each character folder)
         ## crop_directory: Where the cropped images and annotations will go
     
     image_filenames = glob(os.path.join(images_parent_directory, "*.tiff"))
@@ -23,15 +23,18 @@ def harvest(images_parent_directory, annotations_parent_directory, crop_director
         try:
             ## get the stripped filename
             file_stripped_name = filename.split(os.sep)[-1].split('.')[0]
-            annotation_filename = os.path.join(annotations_parent_directory, file_stripped_name + ".npy")
+            
             image = Image.open(filename)
             width, height = image.size
 
             ## start image cropping
             save_cropped_image(np.array(image), width, height, file_stripped_name, crop_directory)
-            
-            ## start annotation cropping
-            save_cropped_annotations(annotation_filename, width, height, crop_directory)
+
+            for annotation_folder in filter(os.path.isdir, os.listdir(annotations_parent_directory)):
+                ## get the corresponding annotation filename
+                annotation_filename = os.path.join(annotations_parent_directory, annotation_folder, "current", file_stripped_name + ".npy")
+                ## start annotation cropping
+                save_cropped_annotations(annotation_filename, width, height, os.path.join(crop_directory, annotation_folder))
 
             print("Cropped: ", file_stripped_name)
         except Exception as e:
@@ -42,8 +45,7 @@ if __name__ == "__main__":
     images_parent_directory = sys.argv[1]
     annotations_parent_directory = sys.argv[2]
     crop_directory = sys.argv[3]
-
-    ## need to create the cropped images and annotations directory
+    
     if not os.path.exists(crop_directory):
         os.mkdir(crop_directory)
         os.mkdir(os.path.join(crop_directory, "images"))
--- DataWrangling/harvest_crops.sh
+++ DataWrangling/harvest_crops.sh
@@ -1,1 +1,1 @@
-python harvest_crops.py "/home/sgkelley/data/maps" "/mnt/nfs/work1/elm/ray/all_char_anots/char_anots_A/current" "/mnt/nfs/work1/elm/sgkelley/cropped"
+python harvest_crops.py "/home/sgkelley/data/maps" "/mnt/nfs/work1/elm/ray/all_char_anots/" "/mnt/nfs/work1/elm/sgkelley/shishir/cropped"
