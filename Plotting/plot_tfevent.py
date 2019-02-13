63dd1f70fd8073f95f6f7d753a34655e5fc3c20d
Shishir Jakati <shishir.jakati98@gmail.com>
2019-02-13 16:21:05 -0500

    Simple change

--- Plotting/plot_tfevent.py
+++ Plotting/plot_tfevent.py
@@ -5,5 +5,6 @@ path_to_events_file = sys.argv[1]
 
 for e in tf.train.summary_iterator(path_to_events_file):
     for v in e.summary.value:
-        if v.tag == 'loss':
-            print(v.simple_value)
+        print(v)
+        # if v.tag == 'loss':
+        #     print(v.simple_value)
