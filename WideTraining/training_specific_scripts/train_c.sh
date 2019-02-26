b4408bde78306f15ab494e5d7c1b4e64ad51bd47
Shishir Jakati <shishir.jakati98@gmail.com>
2019-02-26 10:49:00 -0500

    Define specific training scripts

---
+++ WideTraining/training_specific_scripts/train_c.sh
@@ -0,0 +1,59 @@
+set -x
+set -e
+export CUDA_VISIBLE_DEVICES=$1
+IMG_PER_GPU=$2
+
+TRAIN_DIR=/mnt/nfs/work1/elm/sgkelley/shishir/models/pixel_link/char_anots_c
+SPLIT_NAME=c
+
+# get the number of gpus
+OLD_IFS="$IFS" 
+IFS="," 
+gpus=($CUDA_VISIBLE_DEVICES) 
+IFS="$OLD_IFS"
+NUM_GPUS=${#gpus[@]}
+
+# batch_size = num_gpus * IMG_PER_GPU
+BATCH_SIZE=`expr $NUM_GPUS \* $IMG_PER_GPU`
+
+#DATASET=synthtext
+#DATASET_PATH=SynthText
+
+DATASET=maptext
+DATASET_DIR=/mnt/nfs/work1/elm/sgkelley/shishir/cropped/train_split/images/
+
+python train_pixel_link.py \
+            --train_dir=${TRAIN_DIR} \
+            --num_gpus=${NUM_GPUS} \
+            --learning_rate=1e-4\
+            --gpu_memory_fraction=-1 \
+            --train_image_width=512 \
+            --train_image_height=512 \
+            --batch_size=${BATCH_SIZE}\
+            --dataset_dir=${DATASET_DIR} \
+            --dataset_name=${DATASET} \
+            --dataset_split_name=${SPLIT_NAME} \
+            --max_number_of_steps=100\
+            --checkpoint_path=${CKPT_PATH} \
+            --using_moving_average=1
+
+python train_pixel_link.py \
+            --train_dir=${TRAIN_DIR} \
+            --num_gpus=${NUM_GPUS} \
+            --learning_rate=1e-4\
+            --gpu_memory_fraction=-1 \
+            --train_image_width=512 \
+            --train_image_height=512 \
+            --batch_size=${BATCH_SIZE}\
+            --dataset_dir=${DATASET_DIR} \
+            --dataset_name=${DATASET} \
+            --dataset_split_name=${SPLIT_NAME} \
+            --checkpoint_path=${CKPT_PATH} \
+            --using_moving_average=1\
+            2>&1 | tee -a ${TRAIN_DIR}/log.log   
+
+
+
+
+                     
+
